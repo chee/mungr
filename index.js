0ccfb7f08c4e7739f4d4639c9333ca5ea1b7428e
chee <chee@snaek.org>
2017-02-10 16:45:01 +0000

    disable strict mode

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-module.exports = function mungr(constructors) {
-  function hasAll(canon, candidate) {
-    let ok = true
-    for (key in canon) {
-      if (!candidate.hasOwnProperty(key)) {
-	ok = false
-      }
+const error = TypeError('lol sorry, the mungr does not match the object')
+
+function hasAll(candidate, canon) {
+  let ok = true
+  for (key in canon) {
+    if (!candidate.hasOwnProperty(key)) {
+      ok = false
     }
-    return ok
   }
-  function check(object1, object2) {
-    return hasAll(object1, object2) && hasAll(object2, object1)
-  }
-  return function mung(object) {
-    if (!check(object, constructors)) throw 'lol sorry, this object does not match the interface'
-    // this way the resultant always still matches the interface
-    const resultant = Object.assign({}, object)
-    for (key in constructors) {
-      if (typeof object[key] != 'undefined') {
-        resultant[key] = constructors[key](object[key])
-      }
+  return ok
+}
+
+module.exports = mungrs => object => {
+  if (!hasAll(mungrs, object)) throw error
+  const resultant = Object.assign({}, object)
+  for (key in mungrs) {
+    if (typeof object[key] != 'undefined') {
+      resultant[key] = mungrs[key](object[key])
     }
-    return resultant
   }
+  return resultant
 }
 
