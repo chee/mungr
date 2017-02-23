b1c4e00f5f62dec2f7375bcabe7a91bf503df166
chee <chee@snaek.org>
2017-02-23 16:08:51 +0000

    use function expressions rather than arrow funcs

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-module.exports = mungrs => object => {
-  var resultant = Object.assign({}, object)
-  for (var key in mungrs) {
-    if (typeof object[key] != 'undefined') {
-      resultant[key] = mungrs[key](object[key])
+module.exports = function(mungrs) {
+  return function(object) {
+    var resultant = Object.assign({}, object)
+    for (var key in mungrs) {
+      if (typeof object[key] != 'undefined') {
+        resultant[key] = mungrs[key](object[key])
+      }
     }
+    return resultant
   }
-  return resultant
 }
